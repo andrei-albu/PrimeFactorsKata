b04a2ee32c5c3965e8db4a3c37f044ac499d5c86
andrei-albu <andrei.z.albu@hotmail.com>
2014-09-16 11:38:24 +0300

    initial project

---
+++ PrimeFactors.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactorsKata
+{
+    class PrimeFactors
+    {
+        private const int MaxSize = 2000;
+
+        public static List<int> Generate(int number)
+        {
+            int count = 0;
+            List<int> primesList = new List<int>(new int[MaxSize]);
+            List<int> result = new List<int>(new int[MaxSize]);
+            int index = 0;
+
+            for (int i = 2; i <= number; i++)
+            {
+                if (IsPrime(i))
+                    primesList[count++] = i;
+            }
+
+            while (true)
+            {
+                if (IsPrime(number))
+                {
+                    result[index++] = number;
+                    break;
+                }
+
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    if ((number % primesList[i]) == 0)
+                    {
+                        result[index++] = primesList[i];
+                        number = number / primesList[i];
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i * i <= number; i++)
+                if (number % i == 0) return false;
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeFactorsKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> resultList;
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            resultList = PrimeFactors.Generate(n);
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                if (resultList[i] == 0)
+                    break;
+                Console.Write(resultList[i]);
+                if (i != resultList.Count - 1)
+                    Console.Write(" * ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
